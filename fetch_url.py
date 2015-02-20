1d1cb8c3810127fe159edca4031e10973f50f30c
Ruhi Agarwal <ruhiagarwal@live.com>
2015-02-19 19:46:43 -0800

    Update fetch_url.py

--- fetch_url.py
+++ fetch_url.py
@@ -1,15 +1,15 @@
 import sys
 inputfile=open(sys.argv[1],'r')
 urls=dict()
+str=sys.argv[2]
 for line in inputfile:
 	words=line.split()
-	str="Content-Type"
 	for index,word in enumerate(words):
 		value=word.find(str)
 		if(value!=-1):
-			type_url=word[13:]
+			type_url=word
 			if type_url not in urls:
-				urls[type_url]=0
+				urls[type_url]=1
 			else:
 				urls[type_url]+=1
 print(urls)
